3960ab37f1e0b40ff2fc80c6d03064c5cce6c7da
Isak Swearingen <isakswearingen@gmail.com>
2017-04-19 21:19:31 -0700

    tried to set as variables and functions.  Need sleep

--- index.js
+++ index.js
@@ -3,32 +3,46 @@
 const fs = require('fs')
 const Buffer = require('buffer').Buffer
 
-// fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
-//   if(err) throw err
-//   // console.log(data.toString());
-//   let textData = data.toString();
-//
-//   let bufferOne = Buffer.from(textData)
-//
-//   console.log(bufferOne.toString('hex', 0, 8))
-// })
+let textOne  = function () {fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
+  if(err) throw err
+  // console.log(data.toString());
+  let textData = data.toString();
 
-fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
+  let bufferOne = Buffer.from(textData)
+  // console.log(bufferOne.toString('hex', 0, 8))
+  return bufferOne.toString('hex', 0, 8)
+
+})
+}
+
+let textTwo = function() {fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
   if(err) throw err
   // console.log(data.toString());
   let textData = data.toString();
 
   let bufferTwo = Buffer.from(textData)
+  // console.log(bufferTwo.toString('hex', 0, 8))
+  return bufferTwo.toString('hex', 0, 8)
 
-  console.log(bufferTwo.toString('hex', 0, 8))
 })
+}
 
-// fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
-//   if(err) throw err
-//   // console.log(data.toString());
-//   let textData = data.toString();
-//
-//   let bufferThree = Buffer.from(textData)
-//
-//   console.log(bufferThree.toString('hex', 0, 8))
-// })
+let textThree = function() {fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
+  if(err) throw err
+  // console.log(data.toString());
+  let textData = data.toString();
+
+  let bufferThree = Buffer.from(textData)
+  // console.log(bufferThree.toString('hex', 0, 8))
+  return bufferThree.toString('hex', 0, 8)
+
+})
+}
+
+setTimeOut(textOne(), 5000)
+textTwo();
+textThree();
+
+console.log(textOne());
+console.log(textTwo());
+console.log(textThree());
